0c22019f2693b314e6c37114b56009b16720501e
RajeevPadala <razeev198@gmail.com>
2023-04-16 17:12:42 +0000

    Visit Countries

---
+++ src/components/VisitedCountries/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const VisitedCountries = props => {
+  const {visitedCountryDetails, deleteVisitCountry} = props
+  const {name, imageUrl, id} = visitedCountryDetails
+
+  const remove = () => {
+    deleteVisitCountry(id)
+  }
+
+  return (
+    <li className="visited-countries-list-item">
+      <div className="visited-countries-container">
+        <img src={imageUrl} alt="thumbnail" className="country-image" />
+        <div className="details-section">
+          <p className="visited-country-title">{name}</p>
+          <button className="delete-button" type="button" onClick={remove}>
+            Remove
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default VisitedCountries
